c1a7753fc3f966b354f5c9154e8a8bd65d59a164
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-01-25 16:20:45 -0500

    Add axios CDN link for http requests

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < ApiController
   # $ curl --data "user[email]=user@example.com&user[password]=password" http://localhost:3000/api/v1/auth
   def create
     user = User.find_by(email: params[:user][:email])
-    if user.valid_password? params[:user][:password]
+    if user && user.valid_password?(params[:user][:password])
       render json: { token: JsonWebToken.encode(sub: user.id) }
     else
       render json: { errors: ["Invalid email or password"]}
